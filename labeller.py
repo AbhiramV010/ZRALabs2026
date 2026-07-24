dad5b70dd1d436bb32668ee89ad1ae990312abd5
24k-zilxh <abhiram.vadali@gmail.com>
2026-07-23 21:34:18 -0400

    added labeller.py

---
+++ labeller.py
@@ -0,0 +1,5 @@
+# this file recieves input (as an image), with coordinates on where to draw the rectangle masks
+# it then draws them using cv2's overlay & returns the completed image
+
+import cv2 
+import PIL 
